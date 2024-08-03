9e8b5ad2e2da94034544e39a56aabe3e4ee5f5d2
kohei sano <nlee9337@gmail.com>
2024-08-03 18:32:58 +0900

    プルダウンメニュー追加

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedLevel = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (_) => SecondPage(level: 2)));
-        },
-        child: const Text("Go to Second Page"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton<int>(
+              value: this.selectedLevel,
+              items: [1, 2, 3].map((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text('Level $value'),
+                );
+              }).toList(),
+              onChanged: (int? newValue) {
+                // Handle dropdown value change
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) => SecondPage(level: 2)));
+              },
+              child: const Text("Go to Second Page"),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
